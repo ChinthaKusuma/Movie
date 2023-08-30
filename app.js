7b719ee198eeabf4d51e5953d313580528c04c9c
kusuma <chinthakusuma25@gmail.com>
2023-08-30 17:30:31 +0000

    update data

--- app.js
+++ app.js
@@ -47,6 +47,20 @@ const initiliazeDbAndServer = async () => {
       const resultDbResponse3 = convertCase(dbResponse3);
       response.send(resultDbResponse3);
     });
+    app.put("/movies/:movieId/", async (request, response) => {
+      const { movieId } = request.params;
+      const movieDetails = request.body;
+      const { directorId, movieName, leadActor } = movieDetails;
+      const putQuery = `update Movie
+        set 
+        movie_id=${movieId},
+        director_id=${directorId},
+        movie_name='${movieName}',
+        lead_actor='${leadActor}'
+        where movie_id=${movieId};`;
+      const dbResponse4 = await db.run(putQuery);
+      response.send("Movie Details Updated");
+    });
   } catch (e) {
     console.log("DB Error");
   }
