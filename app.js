e6b6982cb6a71b6da5b4d8f55712b3996996d17f
kusuma <chinthakusuma25@gmail.com>
2023-08-30 11:27:13 +0000

    list movie_names

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const { open } = require("sqlite");
 const path = require("path");
 const dbPath = path.join(__dirname, "moviesData.db");
 const sqlite3 = require("sqlite3");
-
 let db = null;
 const initiliazeDbAndServer = async () => {
   try {
@@ -15,8 +14,16 @@ const initiliazeDbAndServer = async () => {
     app.listen(3000, () => {
       console.log("Server started");
     });
+    app.get("/movies/", async (request, response) => {
+      const MoviesNamesQuery = `select movie_name from Movie;`;
+      const dbResponse = await db.all(MoviesNamesQuery);
+      response.send(
+        dbResponse.map((eachItem) => ({ movieName: eachItem.movie_name }))
+      );
+    });
   } catch (e) {
     console.log("DB Error");
   }
 };
 initiliazeDbAndServer();
+module.exports = app;
